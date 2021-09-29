2967b83a34b01c45d05723af1e0fd25a6aebe219
jgart <47760695+jgarte@users.noreply.github.com>
2021-09-29 11:58:37 -0400

    Adds manifest

---
+++ guix.scm
@@ -0,0 +1,12 @@
+(specifications->manifest
+  '("python-graphviz"
+    "python-ipywidgets"
+    "python-matplotlib"
+    "python-pandas"
+    "python-scikit-learn"))
+
+;;; package these:
+;;nbdev
+;;azure-cognitiveservices-search-imagesearch
+;;sentencepiece
+;;fastai
